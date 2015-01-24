0e6f618ce0ecf3ac28139bd0cae6dad6f52f2a68
Markus Deutsch <markus.a.deutsch@gmail.com>
2015-01-24 19:50:51 +0100

    Changed project structure to simplify deployment to multiple servers.

--- toe-root/config.template.php
+++ toe-root/config.template.php
@@ -1,7 +1,7 @@
 <?php
 
 $config['key'] = "";
-$config['cookiename'] = "toekey";
+$config['key_analytics'] = "";
 
 $config['db_user'] = "";
 $config['db_password'] = "";
@@ -9,5 +9,7 @@ $config['db_database'] = "";
 $config['db_host'] = "";
 
 $db = @new MySQLi($config['db_host'], $config['db_user'], $config['db_password'], $config['db_database']);
+
+include 'properties.php';
 
 ?>
