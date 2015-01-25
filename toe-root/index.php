d8fe2730539a0cf0fb4792d0bf864f0e8349fd12
Markus Deutsch <markus.a.deutsch@gmail.com>
2015-01-25 17:01:18 +0100

    Log attempts and rewrite URL.

--- toe-root/index.php
+++ toe-root/index.php
@@ -31,7 +31,9 @@ while($row2 = mysqli_fetch_assoc($result2)){
 
 shuffle($answers);
 ?>
-
+<script type="text/javascript">
+    window.history.replaceState("TÖ mit Ö", "TÖ mit Ö", "/toe/");
+</script>
 <div id="header">
     <img src="img/toe.png" alt="TÖ" />
     <h1>TÖ mit Ö</h1>
@@ -43,7 +45,9 @@ shuffle($answers);
         if(isset($_GET['q'])){
             if(md5($_GET['q']) == $_GET['a']){
                 echo '<div class="answer-correct">Richtig!</div>';
+                logAttempt($_GET['q'], true, $_GET['a']);
             } else {
+                logAttempt($_GET['q'], false, $_GET['a']);
                 $qid = mysqli_real_escape_string($db, $_GET['q']);
                 $aq = "SELECT word, explanation FROM vocab WHERE vocabid = '$qid'";
                 $ar = mysqli_query($db, $aq);
--- toe-root/lib.php
+++ toe-root/lib.php
@@ -1,8 +1,20 @@
 <?php
 
 function isLoggedIn(){
+    $config = array();
     include 'config.php';
     return isset($_COOKIE[$config['cookiename']]) && $_COOKIE[$config['cookiename']] == $config['key'];
+}
+
+function logAttempt($vocabid, $correct, $answer){
+    $db = null;
+    include 'config.php';
+    $vocabid = mysqli_real_escape_string($db, $vocabid);
+    $correct = $correct ? 1 : 0;
+    $answer = mysqli_real_escape_string($db, $answer);
+
+    $query = "INSERT INTO attempts (fk_vocabid, correct, answer) VALUES ('$vocabid', $correct, '$answer')";
+    mysqli_query($db, $query);
 }
 
 ?>
